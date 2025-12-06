4c6bbf6f3d8b95ccd8498747c74f3703695e0884
S-Arafin <arafin23103@gmail.com>
2025-12-07 03:34:10 +0600

    add api for deleting issue

--- index.js
+++ index.js
@@ -101,8 +101,14 @@ async function run() {
         res.send(result);
     });
 
-   
+    app.delete("/issues/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await issuesCollection.deleteOne(query);
+        res.send(result);
+    });
 
+   
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
